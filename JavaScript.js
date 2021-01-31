5ffede51e93331a702bdc47e8131dd7c500e6746
ponynice <2442775148@qq.com>
2021-01-31 20:26:11 +0800

    任务清单列表
    VUE实现，有添加删除修改的功能

---
+++ JavaScript.js
@@ -0,0 +1,45 @@
+﻿var data = {
+
+    show: false,
+    re: 'reset',
+    newtodo: '',
+    todos: [{
+        title: '学习VUE',
+        check: true
+    },
+    {
+        title: '学习CSS',
+        check: false
+    }]
+}
+new Vue({
+    el: '#container',
+    data: data,
+    methods: {
+        addNewtodo: function () {
+            if (this.newtodo) {
+                this.todos.push({
+                    title: this.newtodo,
+                    check: true
+                })
+            }
+            else {
+                alert("输入不能为空");
+            }
+            this.newtodo = ''
+        },//添加任务并清空输入框
+        adddone: function (d) {
+            d.check = !d.check;
+
+        },
+        hide: function () {
+            this.show = false;
+        },
+        reset: function () {
+            this.show = !this.show;
+            if (this.show) re = 'ok';
+            else re = 'reset';
+        },
+
+    }
+})
